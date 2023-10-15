18dc12349db1277c72152a4ff5414dd1ddd8d66a
Alrick Gicole <alrickgicole@gmail.com>
2023-10-15 22:01:25 +0800

    added students page

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
 
-    return render_template("website.html")
+    return render_template("students.html")
+
+@app.route("/students")
+def students():
+
+    return render_template("students.html")
+
+@app.route("/courses")
+def CCS():
+
+    return render_template("courses.html")
+
     
 
 if __name__ == '__main__':
