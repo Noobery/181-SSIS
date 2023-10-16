4b176fb4e88ed5f656e56ca4d40ef1c54eb397df
Alrick Ivan Gicole <alrickgicole@gmail.com>
2023-10-16 17:27:16 +0800

    Connect sql, display college, and changed to pipenv

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_mysql_connector import MySQL
+
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DATABASE'] = 'student_information_system'
+
+mysql = MySQL(app)
+
 @app.route("/")
 def home_page():
-
     return render_template("students.html")
 
 @app.route("/students")
-def students():
-
+def students():    
     return render_template("students.html")
 
+@app.route("/colleges", methods=["GET", "POST"])
+def colleges():
+    cur = mysql.new_cursor(dictionary=True)
+    
+    if request.method == "POST": 
+        name = request.form.get("collegeName")
+        code = request.form.get("collegeCode")
+        cur.execute("INSERT INTO college (code, name) VALUES (%s, %s)", (code, name))
+        mysql.connection.commit()
+        
+    cur.execute("SELECT * FROM college")
+    colleges = cur.fetchall()
+    
+    return render_template("colleges.html", colleges=colleges)
+
 @app.route("/courses")
-def CCS():
+def courses():
 
     return render_template("courses.html")
-
+    
     
 
-if __name__ == '__main__':
+if __name__ == '__app__':
     app.run (host = '0,0,0,0', debug = True)
