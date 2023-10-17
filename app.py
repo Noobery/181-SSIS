e84e501ec04e915801acea72a6fb67870f54c1bd
Alrick Gicole <alrickgicole@gmail.com>
2023-10-17 12:42:51 +0800

    Overwriting main branch with files from temp-branch

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
-from flask import Flask, render_template, url_for
+from website import app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+"""from flask import Flask, render_template, url_for, request, Blueprint
+from flask_mysql_connector import MySQL
+
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Baptist69!'
+app.config['MYSQL_DATABASE'] = 'SSIS'
+
+mysql = MySQL(app)
+
 @app.route("/")
 def home_page():
-
     return render_template("students.html")
 
 @app.route("/students")
-def students():
-
+def students():    
     return render_template("students.html")
 
+@app.route("/colleges", methods=["GET", "POST"])
+def colleges():
+    cur = mysql.new_cursor(dictionary=True)
+    
+    if request.method == "POST": 
+        name = request.form.get("collegeName")
+        code = request.form.get("collegeCode")
+        cur.execute("INSERT INTO college (code, name) VALUES (%s, %s)", (code, name))
+        mysql.connection.commit()
+        
+    cur.execute("SELECT * FROM college")
+    colleges = cur.fetchall()
+    
+    return render_template("colleges.html", colleges=colleges)
+
 @app.route("/courses")
-def CCS():
+def courses():
 
     return render_template("courses.html")
-
+    
     
 
-if __name__ == '__main__':
+if __name__ == '__app__':
     app.run (host = '0,0,0,0', debug = True)
+
+    """
---
+++ website/routes/collegeRoute.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from website.models.collegeModels import CollegeModel
+
+collegeRoute = Blueprint('college', __name__)
+college_model = CollegeModel()
+
+@collegeRoute.route("/colleges", methods=["GET", "POST"])
+def colleges():
+    if request.method == "POST":
+        name = request.form.get("collegeName")
+        code = request.form.get("collegeCode")
+        college_model.create_college(name, code)
+
+    colleges = college_model.get_colleges()
+    return render_template("colleges.html", colleges=colleges)
+
+@collegeRoute.route("/colleges/delete/<string:college_code>", methods=["DELETE"])
+def delete_college(college_code):
+    result = college_model.delete_college(college_code)
+    return jsonify({'success': result == 'College deleted successfully'})
+
